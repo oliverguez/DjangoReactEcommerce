554bc6cd2c3f900e7d067f44b28929e70c72190b
oliverguez <oliverguez9957@gmail.com>
2021-05-26 19:16:53 -0700

    Remove dir

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+import React, { useState, useEffect } from "react";
+import products from "../products";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
+
+function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data } = await axios.get("http://127.0.0.1:8000/products/");
+      setProducts(data);
+    }
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default HomeScreen;
